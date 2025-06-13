1802952c988fc8a5a39d42c155ad7189772c7ee1
Adam Hooven <71411552+groovytacocat@users.noreply.github.com>
2025-06-13 19:53:48 -0400

    Pushed before saving changes. Adding the answer for 2.41 and the 'scaffolding' for 2.42

--- Exercises/Chapter 2/Section-2.rkt
+++ Exercises/Chapter 2/Section-2.rkt
@@ -721,8 +721,73 @@ Write a procedure to find all ordered triples of distinct positive integers i, j
 a given integer n that sum to a given integer s.
 |#
 
-(define (ordered-triples n s)
-  )
+(define (sum-triples n sum)
+  (filter (lambda (trip) 
+            (= (accumulate + 0 trip) sum))
+          (flatmap (lambda (i)
+             (map (lambda (j) (cons i j))
+                  (unique-pairs (- i 1))))
+           (enumerate-interval 1 n))))
 
-(define ()
-  )
+#|
+Exercise 2.42
+The "Eight-Queens Puzzle" asks how to place eight queens on a chessboard so that no queen is in check from any other (i.e., no two queens are in the same row, column, or diagonal).
+One possible solution is shown in figure 2.8. One way to solve the puzzle is to work across the board, placing a queen in each column. Once we have placed k-1 queens, we must place
+the kth queen in a position where it does not check any of the queens already on the board. We can formulate this approach recursively:
+Assume that we have already generated the sequence of all possible ways to place k-1 queens in the first k-1 columns of the board. For each of these ways, generate an extended set of positions
+by placing a queen in each row of the kth column. Now filter these, keeping only the queens for which the queen in the kth column is safe with respect to the other queens.
+This produces the sequence of all ways to place k queens in the first k columns. By continuing this process, we will produce not only one solution, but all solutions to the puzzle.
+We implement this solution as a procedure queens, which returns a sequence of all solutions to the problem of placing n queens on an n x n chessboard. Queens has an internal procedure called
+queen-cols that returns the sequence of all ways to place queens in the first k columns of the board.
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter 
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+In this procedure rest-of-queens is a way to place k-1 queens in the first k-1 columns, and new-row is a proposed row in which to place the queen for the kth column. 
+Complete the program by implementing the representation for sets of board positions, including the procedure adjoin-position, which adjoins a new row-column position to a set of positions, 
+and empty-board, which represents an empty set of positions. You must also write the procedure safe?, which determines for a set of positions, whether the queen in the kth column is safe 
+with respect to the others. (note that we need only check whether the new queen is safe -- the other queens are already guaranteed safe with respect to each other)
+|#
+
+; representation for sets of board positions
+; (list x y z ...) x = row pos of queen in col 1, y = row pos of queen in col 2, z = row pos of queen in col 3, etc ...
+
+; rest-of-queens is a way to place k-1 queens in the first k-1 columns
+; Sequence of positions (list of lists)
+
+; new-row is a proposed row in which to place the queen for the kth column
+
+; adjoin-position, which adjoins a new row-column position to a set of positions,
+(define (adjoin-position new-position queen rest)
+  nil)
+
+; empty-board, which represents an empty set of positions
+(define empty-board nil)
+
+; safe?, which determines for a set of positions, whether the queen in the kth column is safe with respect to the others. (note that we need only check whether the new queen is safe -- the other queens are already guaranteed safe with respect to each other)
+(define (safe? queen positions)
+  nil)
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter 
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row) (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
